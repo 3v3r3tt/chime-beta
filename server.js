bc5f2d68da0c47f9182b0dc3737c378ac09a1926
Paul Morgan <pmorgan@Pauls-MacBook-Pro-2.local>
2015-12-02 17:49:43 -0700

    master: Added authentication to node.js API

--- server.js
+++ server.js
@@ -1,41 +1,152 @@
 var express = require('express');
-var path = require('path');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+
+var User = require('./app/models/user');
 
 var app = express();
-var adminRouter = express.Router()
+var apiRouter = express.Router();
+
+var port = process.env.PORT || 8080;
+var superSecret = 'ilovescotchscotchyscotchscotch'
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect('localhost:27017/chimeDatabase');
+
+app.use(function(req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  next();
+});
+
+app.use(morgan('dev'));
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.send('Welcome to the home page!');
 });
 
-adminRouter.use(function(req, res, next){
-  console.log(req.method, req.url);
-  next();
+apiRouter.post('/authenticate', function(req, res){
+  User.findOne({
+    username: req.body.username
+  }).select('name username password').exec(function(err, user){
+    if(err) throw err;
+    if(!user) {
+      res.json({
+        success: false,
+        message: 'Authentication failed. User not found.'
+      })
+    } else {
+      var validPassword = user.comparePassword(req.body.password);
+      if(!validPassword){
+        res.json({
+          success: false,
+          message: 'Authentication failed. Wrong password'
+        })
+      } else {
+        var token = jwt.sign({
+          name: user.name,
+          username: user.username
+        }, superSecret, {
+          expiresInMinutes: 1440
+        });
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+  });
 });
 
-adminRouter.get('/', function(req, res){
-  res.send("This is the Dashboard!");
+apiRouter.use(function(req, res, next){
+  var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+  if(token){
+    jwt.verify(token, superSecret, function(err, decoded){
+      if(err){
+        return res.status(403).send({
+          success: false,
+          message: 'Failed to authenticate token.'
+        });
+      } else {
+        req.decoded = decoded;
+        next()
+      }
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+  }
 });
 
-adminRouter.get('/users', function(req, res){
-  res.send("This shows all the users!");
+apiRouter.get('/', function(req, res){
+  res.json({ message: 'Dis dat API shit!' })
 });
 
-adminRouter.param('name', function(req, res, next, name){
-  console.log("validating name: " + name + "....");
-  req.name = name;
-  next();
+apiRouter.get('/me', function(req, res){
+  res.send(req.decoded)
 });
 
-adminRouter.get('/users/:name', function(req, res){
-  res.send("Hello " + req.params.name + "!")
-});
+// Define User Routes
+apiRouter.route('/users')
+  .post(function(req, res){
+    var user = new User();
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.save(function(err){
+      if(err){
+        if(err.code == 11000){
+          return res.json({success: false, message: 'A user with that username already exists!'});
+        } else{
+          return res.send(err);
+        }
+      }
+      res.json({message: 'User created!'});
+    });
+  })
+  .get(function(req, res){
+    User.find(function(err, users){
+      if(err) res.send(err);
+      res.json(users);
+    });
+  });
+apiRouter.route('/users/:user_id')
+  .get(function(req, res){
+    User.findById(req.params.user_id, function(err, user){
+      if(err) res.send(err);
+      res.json(user);
+    });
+  })
+  .put(function(req, res){
+    User.findById(req.params.user_id, function(err, user){
+      if(err) res.send(err);
+      if(req.body.name) user.name = req.body.name;
+      if(req.body.username) user.username = req.body.username;
+      if(req.body.password) user.password = req.body.password;
+      user.save(function(err){
+        if(err) res.send(err);
+        res.json({ message: 'User updated!' });
+      })
+    })
+  })
+  .delete(function(req, res){
+    User.remove({
+      _id: req.params.user_id
+    }, function(err, user){
+      if(err) return res.send(err);
+      res.json({message: 'Successfully deleted!'});
+    });
+  })
 
-adminRouter.get('/posts', function(req, res){
-  res.send("Show all posts")
-});
+app.use('/api', apiRouter);
 
-app.use('/admin', adminRouter);
-
-app.listen(1337);
-console.log("running on port 1337")
+app.listen(port);
+console.log('Magic happens on port: ' + port);

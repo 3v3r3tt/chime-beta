4398be8d2cac2f578aa2e9757dc64ffb0ddeadc9
Paul Morgan <pmorgan@Pauls-MacBook-Pro-2.local>
2015-12-08 14:14:52 -0700

    master: User index, edit/create layouts and routes added

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -14,6 +14,25 @@ angular.module('app.routes', ['ngRoute'])
 			templateUrl: 'app/views/pages/login.html',
 			controller: 'mainController',
 			controllerAs: 'login'
+		})
+
+		// show all users
+		.when('/users', {
+			templateUrl: 'app/views/pages/users/all.html',
+			controller: 'userController',
+			controllerAs: 'user'
+		})
+
+		.when('/users/create', {
+			templateUrl: 'app/views/pages/users/single.html',
+			controller: 'userCreateController',
+			controllerAs: 'user'
+		})
+
+		.when('/users/:user_id', {
+			templateUrl: 'app/views/pages/users/single.html',
+			controller: 'userEditController',
+			controllerAs: 'user'
 		});
 
 	$locationProvider.html5Mode(true);

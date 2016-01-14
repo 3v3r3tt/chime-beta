3302a7a7a8c69a1bf31ed92bb0958f275bb3206b
Paul Morgan <pmorgan@Pauls-MacBook-Pro-2.local>
2016-01-14 14:02:42 -0700

    master: Styling and layout for the landing page

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -18,31 +18,24 @@ angular.module('mainCtrl', [])
 			});
 	});
 
-	// function to handle login form
 	vm.doLogin = function() {
 		vm.processing = true;
-
-		// clear the error
-		vm.error = '';
+		vm.error = ''; // clear the error
 
 		Auth.login(vm.loginData.username, vm.loginData.password)
 			.success(function(data) {
 				vm.processing = false;
-
 				// if a user successfully logs in, redirect to users page
 				if (data.success)
 					$location.path('/users');
 				else
 					vm.error = data.message;
-
 			});
 	};
 
-	// function to handle logging out
 	vm.doLogout = function() {
 		Auth.logout();
 		vm.user = '';
-
 		$location.path('/login');
 	};
 

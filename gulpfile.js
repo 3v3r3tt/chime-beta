225fe69c45240b860cbe2df07f2f7e65c965d55d
Paul M <paul@deyrd.com>
2016-01-16 11:50:37 -0700

    master: Now using minified css. Added node_modules to .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 // Load plugins
 var gulp = require('gulp');
 var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 // Define a task called css
 gulp.task('css', function() {
   //grab the less file, process the less, save to style.css
   return gulp.src('public/assets/css/style.less')
     .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('public/assets/css'));
 });

51a5fe9a3b117d65c459b0a833a746a19a6b68c6
Paul M <paul@deyrd.com>
2016-01-19 14:14:10 -0700

    master: Use gulp to watch for changes and start server

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,28 @@ var rename = require('gulp-rename');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var ngAnnotate = require('gulp-ng-annotate');
+var nodemon = require('gulp-nodemon');
 
-gulp.task('scripts', function() {
-  return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('all.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('public/dist'));
+// Nodemon to run a server
+gulp.task('nodemon', function() {
+  nodemon({
+    script: 'server.js',
+    ext: 'js less html'
+  })
+    .on('start', ['watch'])
+    .on('change', ['watch'])
+    .on('restart', function() {
+      console.log("Restarted....");
+    });
+});
+
+gulp.task('default', ['nodemon']);
+
+// Watch for changes
+gulp.task('watch', function() {
+  gulp.watch('public/assets/css/style.less', ['css']);
+  gulp.watch(['server.js', 'public/app/*.js', 'public/app/**/*.js'], ['js', 'angular']);
 });
 
 // CSS Task
@@ -31,4 +45,23 @@ gulp.task('js', function() {
   return gulp.src(['server.js', 'public/app/*.js', 'public/app/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist'));
+});
+
+gulp.task('angular', function() {
+  return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(ngAnnotate())
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist'));
 });

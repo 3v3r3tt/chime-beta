ec84026b0bf5f4f620bfe3454ed0a7478e510775
Paul M <paul@deyrd.com>
2016-01-21 16:21:16 -0700

    feature/add-sound-cloud-support: Added angular bootstrap to project

--- public/app/app.js
+++ public/app/app.js
@@ -8,7 +8,8 @@ angular.module('userApp', [
   'interestedUserCtrl',
   'userService',
   'chimeService',
-  'interestedUserService'])
+  'interestedUserService',
+  'ui.bootstrap'])
 
   .config([
     '$httpProvider',

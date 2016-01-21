f8bacf22acadfc33814b6ccf82f77ab8024b0b41
Paul M <paul@deyrd.com>
2016-01-21 14:35:46 -0700

    master: Addressing landing pages changes desired by adil

--- app/models/interestedUser.js
+++ app/models/interestedUser.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var InterestedUserSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String },
   email: { type: String, required: true}
 });
 

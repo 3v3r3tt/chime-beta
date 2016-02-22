54621472330f81c7a02e619f0b3a6d5c425cb8c4
Paul M <paul@deyrd.com>
2016-02-21 20:22:23 -0700

    master: Point back to cloud hosted mongo db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'localhost:27017/test',
-	// 'database': 'mongodb://paul:estatewy1@apollo.modulusmongo.net:27017/aqYrus7e',
+	// 'database': 'localhost:27017/test',
+	'database': 'mongodb://paul:estatewy1@apollo.modulusmongo.net:27017/aqYrus7e',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };
